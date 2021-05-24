8f58523bed0cc26b5c91284db257cdbdd416a152
merelmourik <merelmourik@gmail.com>
2021-05-24 12:17:17 +0200

    pretty validation

--- sudoku.c
+++ sudoku.c
@@ -1,19 +1,15 @@
 #include "sudoku.h"
 
 void validate_input(char *str) {
-    int j = 0;
-
     if (strlen(str) != 89)
         invalid_input();
-    for (int i = 0; str[i] != '\0' ; i++)
+    for (int i = 0; str[i] != '\0' ; i++) {
         if (str[i] != '.' && str[i] != ' ' && (str[i] < '1' || str[i] > '9'))
             invalid_input();
-    for (int i = 0; str[i - 1] != '\0'; i++) 
-        if (i % 9 == 0 && i != 0) {
-            if (str[i + j] != ' ' && i != 81)
+        if (i % 10 == 0 && i != 0)
+            if (str[i - 1] != ' ')
                 invalid_input();
-            j++;
-        }
+    }
     return ;
 }
 
