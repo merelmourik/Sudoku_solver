3b191146395008061eaaa5ad891bc3eb1f8765f2
merelmourik <merelmourik@gmail.com>
2021-05-28 09:25:17 +0200

    Now it is really working

--- sudoku.c
+++ sudoku.c
@@ -5,35 +5,33 @@ void solve_sudoku(Sudoku *sudoku) {
     int j       = 1;
     int y       = sudoku->empty_box[0][0];
     int x       = sudoku->empty_box[0][1];
+    int box     = 0;
 
-    for (int box = 0; box <= count; box++)
+    while (box <= count)
         while (j < 10) {
             if (check_row(sudoku->raster[y], j) == 0)       
                 if (check_column(sudoku, x, j) == 0)     
                     if (check_box(sudoku->raster, y, x, j) == 0)
                     {    
-                        sudoku->raster[y][x] = j + 48;  
+                        sudoku->raster[y][x] = j + 48;
                         print_sudoku(sudoku->raster);
-                        if (box == count)    
-                            break;
-                        j = 1;
-                        y = sudoku->empty_box[box][0];     
+                        j = 0;
+                        box++;
+                        if (box == count)
+                            return;
+                        y = sudoku->empty_box[box][0];
                         x = sudoku->empty_box[box][1];     
-                        break;
                     }
             j++;
             if (j == 10) {
                 if (box == 0)
                     return (unsolvable_sudoku());
-                if (box == 1)
-                    box--;
                 else
-                    box -= 2;    
+                    box--;
                 sudoku->raster[y][x] = '.';
                 y = sudoku->empty_box[box][0];
                 x = sudoku->empty_box[box][1]; 
                 j = sudoku->raster[y][x] - 48;
-                break ;
             }
         }
 }
